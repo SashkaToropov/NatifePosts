b8e01eae2ca3629c7d5c2de532d4448252cd3413
SashkaToropov <toropov.oleksandr03@gmail.com>
2023-11-20 17:00:47 +0200

    Initial Commit

---
+++ NatifePosts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifePosts
+//
+//  Created by  Toropov Oleksandr on 20.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
