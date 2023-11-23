33fbe3833dd0d9b0c37b5f7dc419188785314866
SashkaToropov <toropov.oleksandr03@gmail.com>
2023-11-23 23:50:22 +0200

    Set the project
    Organized folder structure, deleted storyboard, and set MainViewController as root view controller.

---
+++ NatifePosts/Application/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  NatifePosts
+//
+//  Created by  Toropov Oleksandr on 20.11.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window?.makeKeyAndVisible()
+    }
+}
